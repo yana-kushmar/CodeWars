1f27e27e0150166ec6cbb9cf82c2c8218d615205
yana-kushmar <yana.puzevich@gmail.com>
2023-09-10 19:10:48 +0300

    Is this my tail?
    Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
    
    If the tail is right return true, else return false.
    
    The arguments will always be non empty strings, and normal letters.

---
+++ kata_105.js
@@ -0,0 +1,14 @@
+
+function correctTail(body, tail){
+
+    sub = body.substr(body.length-tail.length)
+
+    if (sub === tail){
+        return true
+    }else {
+
+        return false
+    }
+
+
+} ;
